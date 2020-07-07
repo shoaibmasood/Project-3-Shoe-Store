d7fe038728d767eafd0994366d52ec72b84c9079
Shoaib Masood <shoaib_masood@rocketmail.com>
2020-07-08 02:25:14 +0500

    added paper component and background gradient

---
+++ src/components/Papper/Papper.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  paper: {
+    dsiplay: 'flex',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+
+    height: '85vh',
+    width: '95vw',
+    backgroundColor: '#fefbec',
+    borderRadius: 15,
+  },
+});
+
+export default function Papper() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Paper elevation={10} className={classes.paper} />
+    </div>
+  );
+}
